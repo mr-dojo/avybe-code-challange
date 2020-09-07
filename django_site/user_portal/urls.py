5c927068bf68163c442b399ea560707b922b58e1
Carlo Costantino <costantinoart@gmail.com>
2020-09-06 15:18:11 -1000

    Added new views

--- django_site/user_portal/urls.py
+++ django_site/user_portal/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('home', views.home, name='home'),
+    path('new-account', views.create_account, name='create account'),
+    path('edit-account', views.edit_account, name='edit account'),
+    path('login', views.login, name='login'),
 ]
--- django_site/user_portal/views.py
+++ django_site/user_portal/views.py
@@ -3,4 +3,21 @@ from django.http import HttpResponse
 
 
 def landing(request):
-    return HttpResponse('<h1>This is the landing page</h1>')
+    return HttpResponse('<h1>This is the landing page</h1><p>You can either log in or create account</p>')
+
+
+def home(request):
+    output = '<h1>Hello %s</h1>'
+    return HttpResponse(output)
+
+
+def create_account(request):
+    return HttpResponse('<h1>Enter nickname and image</h1>')
+
+
+def edit_account(request):
+    return HttpResponse('<h1>Edit your account</h1>')
+
+
+def login(request):
+    return HttpResponse('<h1>Login</h1>')
