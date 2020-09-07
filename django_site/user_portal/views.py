e9b365724f0d38690bc96003a124d9092863872e
Carlo Costantino <costantinoart@gmail.com>
2020-09-07 12:43:25 -1000

    Added update account and logout funcionality

--- django_site/user_portal/views.py
+++ django_site/user_portal/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Person
 from .models import UserState
 
+# _________PAGES_______________
+
 
 def home(request):
     try:
@@ -17,42 +19,66 @@ def create_account(request):
     return render(request, 'user_portal/new-account.html')
 
 
-def save_account(request):
-    try:
-        user_nickname = request.POST['nickname']
-    except (KeyError, user_nickname.DoesNotExist):
-        return render(request, 'user_portal:create account.html', {'error_message': "You didn't tell me your nickname, don't be shy"})
-    else:
-        new_person = Person(nickname=user_nickname)
-        new_person.save()
-        login_as = UserState(current_user=user_nickname)
-        login_as.save()
-        return HttpResponseRedirect('/')
-
-
 def edit_account(request):
-    return HttpResponse('<h1>Edit your account</h1>')
+    try:
+        current_user = UserState.objects.get().current_user
+    except UserState.DoesNotExist:
+        return render(request, 'user_portal/home.html')
+
+    else:
+        return render(request, 'user_portal/edit-account.html', {'current_user': current_user})
 
 
 def login(request):
     return render(request, 'user_portal/login.html')
 
 
+# ________ACTIONS_________
+
+
+def save_account(request):
+    new_user_nickname = request.POST['nickname']
+    # Add validation of nickname (is the nickname taken?)
+    # Create Person
+    new_person = Person(nickname=new_user_nickname)
+    new_person.save()
+    # Update UserState
+    login_as = UserState(current_user=new_user_nickname)
+    login_as.save()
+    return HttpResponseRedirect('/')
+
+
+def update_account(request):
+    new_user_nickname = request.POST['nickname']
+    # Add validation of new nickname (is the new nickname taken?)
+    # Update UserState
+    current_user_nickname = UserState.objects.get().current_user
+    current_logged_in_user = UserState.objects.get()
+    current_logged_in_user.current_user = new_user_nickname
+    current_logged_in_user.save()
+
+    # Update Person
+    person = Person.objects.get(nickname=current_user_nickname)
+    person.nickname = new_user_nickname
+    person.save()
+    return HttpResponseRedirect('/')
+
+
 def validate_login(request):
-    try:
-        # Remove currently logged in user and check that login nickname exists
-        login_nickname = request.POST['users-nickname']
-        valid_nickname = Person.objects.get(nickname=login_nickname) or 0
+    # Add validation (what happens if nickname is incorrect?)
+    login_nickname = request.POST['users-nickname']
+    valid_nickname = Person.objects.get(nickname=login_nickname) or 0
 
-        current_user = UserState.objects.all()
-        current_user.delete()
+    current_user = UserState.objects.all()
+    current_user.delete()
 
-    except (KeyError, bool(valid_nickname)):
-        return render(request, 'user_portal:login.html', {'error_message': "Your nickname is incorrect"})
-    except (KeyError, login_nickname.DoesNotExist):
-        return render(request, 'user_portal:login.html', {'error_message': "Please enter your nickname"})
+    login_as = UserState(current_user=valid_nickname)
+    login_as.save()
+    return HttpResponseRedirect('/')
 
-    else:
-        login_as = UserState(current_user=valid_nickname)
-        login_as.save()
-        return HttpResponseRedirect('/')
+
+def logout(request):
+    current_user = UserState.objects.all()
+    current_user.delete()
+
+    return HttpResponseRedirect('/')
